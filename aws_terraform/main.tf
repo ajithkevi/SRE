073e7684c8e32542d07a88175446ef691e69beb0
Ajith Kv <ajith@FMIC00180.local>
2022-11-28 10:12:40 +0530

    OV-1234: Add other network

--- aws_terraform/main.tf
+++ aws_terraform/main.tf
@@ -15,7 +15,7 @@ module "vpc" {
   cidr = "10.1.0.0/16"
 
   azs             = ["${var.aws_region}a"]
-  private_subnets = ["10.1.1.0/24"]
+  private_subnets = ["10.1.1.0/24, 10.2.0.0/16"]
   public_subnets  = ["10.1.101.0/24"]
 
   enable_nat_gateway   = true
